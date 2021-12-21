b838d836828bc811665d96439293165f4f0ca3d6
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-12-21 09:21:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+const TypeDocGenerator = require('./lib/typedoc-generator');
+
+
+module.exports = {
+  name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    const addonOptions = (this.parent && this.parent.options) || (app && app.options) || {};
+    this.addonConfig   = addonOptions[this.name] || {};
+    this.addonEnabled  = this.addonConfig.enabled === true || process.env.EMBER_CLI_TYPEDOC === 'false';
+  },
+
+  async outputReady() {
+    if (this.addonEnabled) {
+      const generator = new TypeDocGenerator();
+
+      const config = Object.assign({
+        entryPoints: [this.project.isEmberCLIAddon() ? './addon' : './app'],
+        entryPointStrategy: 'expand',
+        out: './docs',
+        json: './docs/docs.json',
+        logger: this.ui.writeLine.bind(this.ui),
+      }, this.addonConfig);
+
+      this.ui.writeInfoLine('Running TypeDoc');
+
+      await generator.generate(config);
+    }
+  },
+};
